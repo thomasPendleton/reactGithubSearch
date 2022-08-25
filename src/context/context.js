ede6804d4fabd71336aedc75fb0d8b2769eb8a93
Thomas Pendleton <thomas6576@yahoo.com>
2022-08-25 15:50:19 -0700

    start new project

---
+++ src/context/context.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import mockUser from './mockData.js/mockUser';
+import mockRepos from './mockData.js/mockRepos';
+import mockFollowers from './mockData.js/mockFollowers';
+import axios from 'axios';
+
+const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext()
+//Provider, consumer are accessible after creating context from react.
+// GithubContext.Provider
+
+const GithubProvider = ({children}) => {
+  return (
+    <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>
+  )
+}
+
+export default GithubProvider
